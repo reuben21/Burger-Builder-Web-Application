37f5589ecbbff2b162f7c4c6984bfdf8d58b356d
Reuben Coutinho <reuben21>
2020-07-04 16:44:38 +0530

    retrieving from firebase

--- src/withErrorHandling/withErrorHandling.js
+++ src/withErrorHandling/withErrorHandling.js
@@ -8,17 +8,17 @@ const withErrorHandler =(WrappedComponent,axios)=>{
             error:null
         }
         componentWillMount() {
-            this.reqInterceptor=axios.interceptors.response.use(req=>{
+            this.requestInterceptor=axios.interceptors.response.use(req=>{
                 this.setState({error:null});
                 return req;
             })
-            this.resInterceptor=axios.interceptors.response.use(res=>res,error=>{
+            this.responseInterceptor=axios.interceptors.response.use(res=>res,error=>{
                 this.setState({error:error})
             })
         }
         componentWillUnmount() {
-            axios.interceptors.request.eject(this.reqInterceptor);
-            axios.interceptors.response.eject(this.resInterceptor);
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
 
         errorConfirmedHandler=()=>{
