187704569f9af58d606e14df8dff60b4ea688fd5
Reuben Coutinho <reuben21>
2020-07-04 16:48:26 +0530

    Updated

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,7 +24,7 @@ class BurgerBuilder extends Component {
     state = {
         ingredients: {
             salad: 0,
-            bacon: 0,
+            bacon: 1,
             cheese: 0,
             meat: 0
         },
