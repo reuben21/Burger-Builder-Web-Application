49286559db63004ef5abe892625c4954b1339df2
reuben21 <reuben211999@gmail.com>
2020-07-06 16:30:10 +0530

    Updating Checkout Page with Routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React, {Component} from 'react';
-
+import {Route, Switch} from 'react-router-dom'
 import './App.module.css';
 import Layout from './components/Layout/Layout';
 
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
-class App extends Component{
-    state={
-        show:true
+class App extends Component {
+    state = {
+        show: true
     };
 
-    componentDidMount() {
-        setTimeout(()=>{
-            this.setState({show:false})
-        },5000)
-    }
 
-    render(){
+    render() {
         return (
             <div className="App">
                 <Layout>
-                    <BurgerBuilder/>
-
+                    <Switch>
+                        <Route path="/checkout" component={Checkout}/>
+                        <Route path="/" exact component={BurgerBuilder}/>
+                    </Switch>
                 </Layout>
             </div>
         );
